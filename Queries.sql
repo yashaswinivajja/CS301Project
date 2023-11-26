28b4482fabc3435cc27a4bf73b08d7e09c09f8ea
NiroopmaVerma <115918383+NiroopmaVerma@users.noreply.github.com>
2023-11-27 00:12:58 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,1 +1,40 @@
+insert into Category values (1000, 'Clocks', 'Collection of stylish wall clocks, elegant pendulum clocks, reliable alarm clocks and much more');
+insert into Category values (1001, 'Photo frames', 'A complete range of photo frames in several designs, materials, sizes and works of art');
+insert into Category values (1002, 'Paintings', 'Browse through millions of paintings from classic artists along with independent artists all over the world');
+insert into Category values (1003, 'Artificial flowers', 'Explore the whole collection of Artificial flowers and plants for your home decorations');
+insert into Category values (1004, 'Posters', 'Decorate your home or office with some awesome posters from our collection.');
+insert into Category values (1005, 'Stickers', 'Get rid of the boring and plain look of your accessories and decorate them with quirky stickers');
+insert into Category values (1006, 'Mirrors', 'Decorate you home with elegant and unique designs in mirrors and glasses');
+insert into Category values (1007, 'Wall Hangings', 'Transform interiors of your home with colorful wall hangings at low prices');
+insert into Category values (1008, 'Vases', 'Browse through a range of vases and bowls that are sure to liven up your home.');
+insert into Category values (1009, 'Idols', 'Elevate your sacred space with our exquisite and stunning collections of idols.');
+insert into Category values (1010, 'Candles', 'Buy scented, decorative and colourful to lit up your home.');
+insert into Category values (1011, 'Showpieces', 'Shop figurines, compasses, hourglasses, telescopes, scluptures and miniature replicas.');
+insert into Category values (1012, 'Wind chimes', 'Choose from wood, brass, ceramic windchimes to decorate your home with a best hanging showpiece.');
 
+
+insert into Seller values (1100, 'John Doe', 5234567890, 5034);
+insert into Seller values (2120, 'Jane Smith', 9876543210, 8430);
+insert into Seller values (3120, 'Bob Johnson', 5234243235, 3000);
+insert into Seller values (3210, 'Alice Brown', 9998887777, 7030);
+insert into Seller values (5323, 'Charlie Davis', 4443332222, 3400);
+insert into Seller values (6128, 'Eva White', 1112223333, 9220);
+insert into Seller values (1217, 'David Lee', 6667778888, 40023);
+insert into Seller values (1328, 'Grace Miller', 2223334444, 750);
+
+
+insert into Customer values (3101, 'Alice', 'Mae', 'Johnson', 'alice@email.com', '1990-05-15', 1234567890, 32);
+insert into Customer values (3102, 'Bob', 'Lee', 'Smith', 'bob@email.com', '1985-08-22', 9876543210, 37);
+insert into Customer values (3103, 'Charlie', 'Ray', 'Davis', 'charlie@email.com', '1992-02-10', 5555555555, 30);
+insert into Customer values (3104, 'David', 'John', 'Brown', 'david@email.com', '1980-11-05', 9998887777, 43);
+insert into Customer values (3105, 'Eva', 'Grace', 'White', 'eva@email.com', '1995-07-18', 4443332222, 28);
+insert into Customer values (3106, 'Frank', 'Paul', 'Miller', 'frank@email.com', '1988-09-27', 1112223333, 35);
+insert into Customer values (3107, 'Grace', 'Elizabeth', 'Taylor', 'grace@email.com', '1998-04-03', 6667778888, 25);
+insert into Customer values (3108, 'Henry', 'Thomas', 'Clark', 'henry@email.com', '1983-12-12', 2223334444, 40);
+insert into Customer values (3109, 'Ivy', 'Marie', 'Anderson', 'ivy@email.com', '1993-06-25', 7778889999, 28);
+insert into Customer values (3110, 'Jack', 'Walter', 'Perez', 'jack@email.com', '1987-01-08', 3334445555, 34);
+insert into Customer values (3111, 'Kelly', 'Ann', 'Harris', 'kelly@email.com', '1990-09-14', 8889990000, 31);
+insert into Customer values (3112, 'Liam', 'Michael', 'Martin', 'liam@email.com', '1997-03-21', 5556667777, 24);
+insert into Customer values (3113, 'Mia', 'Nicole', 'Garcia', 'mia@email.com', '1989-07-30', 2221113333, 32);
+insert into Customer values (3114, 'Noah', 'Alexander', 'Cooper', 'noah@email.com', '1994-11-11', 9991112222, 27);
+insert into Customer values (3115, 'Olivia', 'Rose', 'Russell', 'olivia@email.com', '1986-05-08', 6662223333, 36);
